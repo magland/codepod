5c0a15df7f1f853b78437bd68b34c4a4dfaaaa47
Jeremy Magland <jeremy.magland@gmail.com>
2019-02-15 06:00:46 -0500

    incr version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name="codepod"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.0",
+    version="0.1.2",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Open code workspaces in docker for editing via containerized vscode",
